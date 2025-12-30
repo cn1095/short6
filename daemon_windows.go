632cdd4e71543b0b25de75832597c923c04eb57a
cn1095 <1095053105@qq.com>
2025-12-30 20:30:44 +0800

    Update daemon_windows.go

--- daemon_windows.go
+++ daemon_windows.go
@@ -1,18 +1,18 @@
 //go:build windows
 // +build windows
 package main
-  
+
 import (  
     "log"  
     "os/exec"  
-)  
-  
+) 
+
 func runAsDaemonWindows() {
     cmd := exec.Command(os.Args[0], os.Args[1:]...)
     cmd.Env = os.Environ()
-    err := cmd.Start()  
-    if err != nil {  
-        log.Fatalf("后台运行失败: %v", err)  
-    }  
-    os.Exit(0)  
+    err := cmd.Start()
+    if err != nil {
+        log.Fatalf("后台运行失败: %v", err)
+    }
+    os.Exit(0)
 }
